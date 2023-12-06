5f47900348a44969128bb34fdf980814ccbead58
Rafael Blum <rafaelblum_digital@hotmail.com>
2023-12-06 13:51:57 -0300

    :heavy_plus_sign: :lipstick: feat: Add Tailwindcss/landingPage

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,9 @@
 import './bootstrap';
 
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js"
     ],
 
     theme: {
@@ -14,6 +18,9 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#2563eb", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a" }
+            }
         },
     },
 
