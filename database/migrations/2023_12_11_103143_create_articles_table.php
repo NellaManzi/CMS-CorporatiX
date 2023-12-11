fd94e3297c28ffcfa7ee0615f8b494be9a90e068
Rafael Blum <rafaelblum_digital@hotmail.com>
2023-12-11 11:29:28 -0300

    :recycle: :bricks: feat: migrations article/user/category relationship

---
+++ database/migrations/2023_12_11_103143_create_articles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\User::class);
+            $table->string('title');
+            $table->string('slug');
+            $table->string('subTitle')->nullable();
+            $table->string('summary')->nullable();
+            $table->longText('content');
+            $table->dateTime('published_at')->nullable();
+            $table->boolean('status')->default(false);
+            $table->integer('views')->nullable(0);
+            $table->string('cover')->default('storage/default_article.jpg');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,13 +5,17 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ url('/') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="url('/admin')" :active="request()->routeIs('dashboard')">
+                        {{ __('Administração') }}
+                    </x-nav-link>
+
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
